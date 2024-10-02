e805d2f67c11a049da6ed79510cefbe9893a490a
Pcjmy <1716045191@qq.com>
2024-10-02 19:43:03 +0800

    feat: 实现JSX

--- packages/react/src/jsx.ts
+++ packages/react/src/jsx.ts
@@ -1,5 +1,12 @@
 import { REACT_ELEMENT_TYPE } from 'shared/ReactSymbols';
-import { Type, Key, Ref, Props, ReactElementType } from 'shared/ReactTypes';
+import {
+	Type,
+	Key,
+	Ref,
+	Props,
+	ElementType,
+	ReactElementType
+} from 'shared/ReactTypes';
 
 // ReactElement
 
@@ -19,3 +26,42 @@ const ReactElement = function (
 	};
 	return element;
 };
+
+export const jsx = (type: ElementType, config: any, ...maybeChildren: any) => {
+	let key: Key = null;
+	let ref: Ref = null;
+	const props: Props = {};
+
+	for (const prop in config) {
+		const val = config[prop];
+		if (prop === 'key') {
+			if (val !== undefined) {
+				key = '' + val;
+			}
+			continue;
+		}
+		if (prop === 'ref') {
+			if (val !== undefined) {
+				ref = val;
+			}
+			continue;
+		}
+		if ({}.hasOwnProperty.call(config, prop)) {
+			props[prop] = val;
+		}
+	}
+
+	const maybeChildrenLength = maybeChildren.length;
+
+	if (maybeChildrenLength) {
+		if (maybeChildrenLength === 1) {
+			props.children = maybeChildren[0];
+		} else {
+			props.children = maybeChildren;
+		}
+	}
+
+	return ReactElement(type, key, ref, props);
+};
+
+export const jsxDev = jsx;
