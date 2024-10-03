1946dad55cbb4abf9eaf8c5ef26d9a7be1b79284
Pcjmy <1716045191@qq.com>
2024-10-03 17:59:16 +0800

    feat: FiberNode

---
+++ packages/react-reconciler/src/fiber.ts
@@ -0,0 +1,38 @@
+import { Props, Key, Ref } from 'shared/ReactTypes';
+import { WorkTag } from './workTags';
+
+export class FiberNode {
+	type: any;
+	tag: WorkTag;
+	pendingProps: Props;
+	key: Key;
+	stateNode: any;
+	ref: Ref;
+
+	return: FiberNode | null;
+	sibling: FiberNode | null;
+	child: FiberNode | null;
+	index: number;
+
+	memoizedProps: Props | null;
+
+	constructor(tag: WorkTag, pendingProps: Props, key: Key) {
+		// 实例
+		this.tag = tag;
+		this.key = key;
+		this.stateNode = null;
+		this.type = null;
+
+		// 构成树状结构
+		this.return = null;
+		this.sibling = null;
+		this.child = null;
+		this.index = 0;
+
+		this.ref = null;
+
+		// 作为工作单元
+		this.pendingProps = pendingProps;
+		this.memoizedProps = null;
+	}
+}
