c4a16989a0f914ce492d60949347c780af2b5f36
Pcjmy <1716045191@qq.com>
2024-10-03 19:32:35 +0800

    feat: fiberFlags

--- packages/react-reconciler/src/fiber.ts
+++ packages/react-reconciler/src/fiber.ts
@@ -1,5 +1,6 @@
 import { Props, Key, Ref } from 'shared/ReactTypes';
 import { WorkTag } from './workTags';
+import { Flags, NoFlags } from './fiberFlags';
 
 export class FiberNode {
 	type: any;
@@ -15,6 +16,8 @@ export class FiberNode {
 	index: number;
 
 	memoizedProps: Props | null;
+	alternate: FiberNode | null;
+	flags: Flags;
 
 	constructor(tag: WorkTag, pendingProps: Props, key: Key) {
 		// 实例
@@ -34,5 +37,10 @@ export class FiberNode {
 		// 作为工作单元
 		this.pendingProps = pendingProps;
 		this.memoizedProps = null;
+
+		this.alternate = null;
+
+		// 副作用
+		this.flags = NoFlags;
 	}
 }
