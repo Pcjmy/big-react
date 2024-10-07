ac6c19b6f841ddc047d281bb69a371bd230ab92b
Pcjmy <1716045191@qq.com>
2024-10-07 23:43:22 +0800

    feat: 实现Hooks架构

--- packages/react-reconciler/src/fiberHooks.ts
+++ packages/react-reconciler/src/fiberHooks.ts
@@ -1,9 +1,33 @@
+import internals from 'shared/internals';
 import { FiberNode } from './fiber';
 
+let currentlyRenderingFiber: FiberNode | null = null;
+let workInPropgressHook: Hook | null = null;
+
+const { currentDispatcher } = internals;
+
+interface Hook {
+	memoizedState: any;
+	updateQueue: unknown;
+	next: Hook | null;
+}
+
 export function renderWithHooks(wip: FiberNode) {
+	currentlyRenderingFiber = wip;
+	wip.memoizedState = null;
+
+	const current = wip.alternate;
+
+	if (current !== null) {
+		// update
+	} else {
+		// mount
+	}
+
 	const Component = wip.type;
 	const props = wip.pendingProps;
 	const children = Component(props);
+	currentlyRenderingFiber = null;
 
 	return children;
 }
