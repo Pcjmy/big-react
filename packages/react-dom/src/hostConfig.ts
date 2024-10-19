0ae3112dfdd0fbe4427e741edc1fad0c645769c9
Pcjmy <1716045191@qq.com>
2024-10-19 18:22:21 +0800

    feat: update流程commit阶段

--- packages/react-dom/src/hostConfig.ts
+++ packages/react-dom/src/hostConfig.ts
@@ -1,5 +1,9 @@
+import { FiberNode } from 'react-reconciler/src/fiber';
+import { HostText } from 'react-reconciler/src/workTags';
+
 export type Container = Element;
 export type Instance = Element;
+export type TextInstance = Text;
 
 export const createInstance = (type: string): Instance => {
 	const element = document.createElement(type);
@@ -18,3 +22,27 @@ export const createTextInstance = (content: string) => {
 };
 
 export const appendChildToContainer = appendInitialChild;
+
+export const commitUpdate = (fiber: FiberNode) => {
+	switch (fiber.tag) {
+		case HostText:
+			const text = fiber.memoizedProps.content;
+			return commitTextUpdate(fiber.stateNode, text);
+		default:
+			if (__DEV__) {
+				console.warn('未实现的Update类型', fiber);
+			}
+			break;
+	}
+};
+
+const commitTextUpdate = (textInstance: TextInstance, text: string) => {
+	textInstance.textContent = text;
+};
+
+export const removeChild = (
+	container: Container,
+	child: Instance | TextInstance
+) => {
+	container.removeChild(child);
+};
